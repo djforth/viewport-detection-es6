26623234512f8e3d6f0d6151020f1b886c10a035
Adrian Stainforth <adrian@morsedigital.com>
2016-03-03 22:01:41 +0000

    fix lodash issue

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var _createClass = (function () { function defineProperties(target, props) { for
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var _ = require("lodash/core");
+_.remove = require("lodash/remove");
 var forIn = require("lodash/forIn");
 
 var ViewportDetection = (function () {
